fc824e7be930dbafda55264adfba5a442f2623c2
boo50 <30969035+boo50@users.noreply.github.com>
2020-09-05 10:24:35 +0700

    Update bot.py

--- bot.py
+++ bot.py
@@ -27,9 +27,9 @@ import gettext
 bot = telebot.TeleBot(config.BotAPIKey)
 # /API Token
 
-#lang_translations = gettext.translation('base', localedir=os.path.join(config.serverbotpath, "locales"), languages=['en'])
-#lang_translations.install()
-#_= lang_translations.gettext
+lang_translations = gettext.translation('base', localedir=os.path.join(config.serverbotpath, "locales"), languages=['en'])
+lang_translations.install()
+_= lang_translations.gettext
 
 # Log
 logger = telebot.logger
